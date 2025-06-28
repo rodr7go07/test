498959079ef0277b8000b6335d67bbd3050952c7
rodr7go <rodr7go.lg@gmail.com>
2025-06-27 19:42:28 -0600

    edit car info done

--- app/Livewire/ListAutos.php
+++ app/Livewire/ListAutos.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use App\Models\Car;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -24,26 +25,56 @@ class ListAutos extends Component
         'is_available' => 'Disponibilidad'
     ];
 
+    public $car_name;
+    public $car_price;
+    public $brand_id;
+    public $car_model;
+    public bool $carUpdated = false;
+
     public function render()
     {
         $cars = Car::with('brand')->paginate(10);
+        $brands = Brand::all();
 
         return view('livewire.cars.list-autos', [
             'cars' => $cars,
             'columns' => $this->columns,
+            'brands' => $brands
         ]);
     }
 
 
     public function editCarModal(Car $car)
     {
-        $this->car = $car;
         $this->showEditCarModal = true;
+        $this->car = $car;
+        $this->car_name = $car->name;
+        $this->car_price = $car->price;
+        $this->brand_id = $car->brand->id;
+        $this->car_model = $car->model;
     }
 
     public function closeModal()
     {
         $this->showEditCarModal = false;
         $this->car = null;
+    }
+
+    public function updateCar()
+    {
+        $this->validate([
+            'car_name' => 'required',
+            'car_price' => 'required',
+            'brand_id' => 'required',
+        ]);
+        $this->car->name = $this->car_name;
+        $this->car->price = $this->car_price;
+        $this->car->brand_id = $this->brand_id;
+        $this->car->model = $this->car_model;
+        $this->car->update();
+
+        $this->closeModal();
+
+        $this->carUpdated = true;
     }
 }
--- resources/views/livewire/cars/modals/edit-car-modal.blade.php
+++ resources/views/livewire/cars/modals/edit-car-modal.blade.php
@@ -1,10 +1,50 @@
 <x-modal wire:model="showEditCarModal" maxWidth="3xl">
     <x-slot name="title">Editar auto {{ $car->name ?? '' }}</x-slot>
 
-    <p>Aquí va el contenido del modal</p>
+
+    <div class="flex gap-x-2">
+        <div class="w-full">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+            <input type="text" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="car_name">
+
+            @error('car_name')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div class="w-full">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
+            <input type="number" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="car_price">
+            @error('car_price')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
+        </div>
+    </div>
+
+    <div class="flex gap-x-2 mt-2">
+
+        <div class="w-full">
+            <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Marca</label>
+            <select id="brand" wire:model="brand_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="">Seleccionar marca</option>
+                @foreach($brands as $brand)
+                    <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                @endforeach
+            </select>
+            @error('brand_id')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div class="w-full">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Modelo</label>
+            <input type="number" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="car_model">
+        </div>
+
+    </div>
 
     <x-slot name="footer">
         <button wire:click="$set('showEditCarModal', false)" class="px-4 py-2 bg-red-500 rounded hover:bg-red-700 text-white">Cancelar</button>
-        <button class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700">Guardar</button>
+        <button wire:click="updateCar" class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700">Guardar</button>
     </x-slot>
 </x-modal>
